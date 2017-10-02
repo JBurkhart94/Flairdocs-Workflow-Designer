41390da61324785bd1baecb2b013d98a4f553313
Joe Burkhart <joe.burkhart12@gmail.com>
2017-10-02 18:01:46 -0400

    Auto Scroll Workflow window

--- Flairdocs-Workflow-Designer/Scripts/workflow-designer.js
+++ Flairdocs-Workflow-Designer/Scripts/workflow-designer.js
@@ -1,11 +1,16 @@
-﻿//Register event listeners when the document loads
+﻿var steps = 0;
+
+//Register event listeners when the document loads
 $(document).ready(function () {
     $('#add-step-button').click(addStep);
 });
 
 //Function to add a step to the workflow design window
-function addStep() {
+function addStep(e) {
+    steps += 1;
+    console.log("Adding Step");
     var code = '<div class="workflow-step"></div >';
     $(code).insertBefore('#add-step-button');
-    $('#workflow-designer-window-edit').scrollLeft($('workflow-designer-window-edit').width());
+    var tag = $('#workflow-designer-window-edit');
+    tag.scrollLeft(350 * steps);
 }
