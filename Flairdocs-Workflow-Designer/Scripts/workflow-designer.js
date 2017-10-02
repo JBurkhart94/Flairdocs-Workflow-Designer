fbb0c96af4269a92d53619d266ee114ea6e236d0
Joe Burkhart <joe.burkhart12@gmail.com>
2017-10-02 13:16:54 -0400

    Added ability to add steps

--- Flairdocs-Workflow-Designer/Scripts/workflow-designer.js
+++ Flairdocs-Workflow-Designer/Scripts/workflow-designer.js
@@ -1,5 +1,11 @@
-﻿$('#test').click(function () {
-    $('#test').css({
-        'background-color': 'red',
-    });
+﻿//Register event listeners when the document loads
+$(document).ready(function () {
+    $('#add-step-button').click(addStep);
 });
+
+//Function to add a step to the workflow design window
+function addStep() {
+    var code = '<div class="workflow-step"></div >';
+    $(code).insertBefore('#add-step-button');
+    $('#workflow-designer-window-edit').scrollLeft($('workflow-designer-window-edit').width());
+}
