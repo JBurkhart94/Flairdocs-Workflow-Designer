b71174804685ebb7ea53b6a3daac6e8954e5b655
Joe Burkhart <joe.burkhart12@gmail.com>
2017-10-04 13:12:52 -0400

    UI changes

--- Flairdocs-Workflow-Designer/Scripts/workflow-designer.js
+++ Flairdocs-Workflow-Designer/Scripts/workflow-designer.js
@@ -1,5 +1,13 @@
-﻿$('#test').click(function () {
-    $('#test').css({
-        'background-color': 'red',
-    });
+﻿var steps = 0;
+
+$(document).ready(function () {
+    $('#add-step-button').click(addStep);
 });
+
+function addStep(e) {
+    steps += 1;
+    var code = '<div class="workflow-step"></div >';
+    $(code).insertBefore('#add-step-button');
+    var tag = $('#workflow-designer-window-edit');
+    tag.scrollLeft(350 * steps);
+}
