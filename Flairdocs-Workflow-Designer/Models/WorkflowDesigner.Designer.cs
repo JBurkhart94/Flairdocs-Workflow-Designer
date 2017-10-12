d35d8b7e98f0aba868a7d74225f797da2588642d
mkoch17 <koch.382@osu.edu>
2017-10-12 15:52:45 -0400

    Added .gitignore and removed files from the git repo

--- Flairdocs-Workflow-Designer/Models/WorkflowDesigner.Designer.cs
+++ Flairdocs-Workflow-Designer/Models/WorkflowDesigner.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\Joe\Desktop\Flairdocs-Workflow-Designer\Flairdocs-Workflow-Designer\Models\WorkflowDesigner.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\Mason\Documents\Classwork\Capstone\TempFlairdocs\Flairdocs-Workflow-Designer\Flairdocs-Workflow-Designer\Models\WorkflowDesigner.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
