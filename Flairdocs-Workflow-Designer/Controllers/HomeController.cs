854d34ce85ba1ae3e3525613f4ce9c657123fec8
Joe Burkhart <joe.burkhart12@gmail.com>
2017-10-31 14:33:05 -0400

    New Workflow Edit and Controller Code

--- Flairdocs-Workflow-Designer/Controllers/HomeController.cs
+++ Flairdocs-Workflow-Designer/Controllers/HomeController.cs
@@ -3,14 +3,72 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Flairdocs_Workflow_Designer.Models;
 
 namespace Flairdocs_Workflow_Designer.Controllers
 {
     public class HomeController : Controller
     {
+
+        WorkflowContext db = new WorkflowContext();
+
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult Workflow(Guid id)
+        {
+            Workflow workflow = db.Workflows.Find(id);
+            return View(workflow);
+        }
+
+        public Guid? Create(String title, String description)
+        {
+            if (!TitleExists(title))
+            {
+                //Create Workflow and AuditLog
+                AuditLog auditLog = new AuditLog
+                {
+                    Id = Guid.NewGuid()
+                };
+                db.AuditLogs.Add(auditLog);
+
+                Workflow workflow = new Models.Workflow
+                {
+                    Id = Guid.NewGuid(),
+                    Title = title,
+                    Description = description,
+                    Creation_Date = DateTime.Now,
+                    AuditLog = auditLog
+                };
+                db.Workflows.Add(workflow);
+
+                //Save
+                db.SaveChanges();
+                return workflow.Id;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        //Check if a workflow exists with the given name
+        public Boolean TitleExists(String title)
+        {
+            var exists = from w in db.Workflows
+                         where w.Title == title
+                         select w;
+
+            if (exists.Count() > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
 
         public ActionResult About()
