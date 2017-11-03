6f6d688f10bbb62f49364da07ba79fabaa1d30e3
Joe Burkhart <joe.burkhart12@gmail.com>
2017-11-03 01:06:17 -0400

    Added some tests

--- Flairdocs-Workflow-Designer.Tests/Controllers/HomeControllerTest.cs
+++ Flairdocs-Workflow-Designer.Tests/Controllers/HomeControllerTest.cs
@@ -7,6 +7,7 @@ using System.Web.Mvc;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Flairdocs_Workflow_Designer;
 using Flairdocs_Workflow_Designer.Controllers;
+using Flairdocs_Workflow_Designer.Models;
 
 namespace Flairdocs_Workflow_Designer.Tests.Controllers
 {
@@ -21,23 +22,61 @@ namespace Flairdocs_Workflow_Designer.Tests.Controllers
 
             // Act
             ViewResult result = controller.Index() as ViewResult;
-            System.Threading.Thread.Sleep(1000);
 
             // Assert
             Assert.IsNotNull(result);
         }
 
         [TestMethod]
-        public void About()
+        public void TitleExists()
         {
-            // Arrange
+            Console.WriteLine("Workflow test");
             HomeController controller = new HomeController();
+            Boolean result = controller.TitleExists("Test Workflow");
+            Assert.IsTrue(result);
+        }
 
-            // Act
-            ViewResult result = controller.About() as ViewResult;
+        [TestMethod]
+        public void Workflow()
+        {
+            WorkflowContext context = new WorkflowContext();
+            var workflows = from w in context.Workflows
+                            where w.Title == "Test Workflow"
+                            select w;
 
-            // Assert
-            Assert.AreEqual("Your application description page.", result.ViewBag.Message);
+            Guid id = workflows.First().Id;
+            HomeController controller = new HomeController();
+            ViewResult result = controller.Workflow(id) as ViewResult;
+            Assert.AreEqual("Test Workflow", result.ViewBag.Title);
+
+        }
+
+        [TestMethod]
+        public void WorkflowSearchExists()
+        {
+            String test = "Test Workflow";
+            HomeController controller = new HomeController();
+            Guid? result = controller.WorkflowSearch(test);
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void WorkflowSearchDoesNotExist()
+        {
+            String test = "";
+            HomeController controller = new HomeController();
+            Guid? result = controller.WorkflowSearch(test);
+            Assert.IsNull(result);
+        }
+
+        [TestMethod]
+        public void CreateRejected()
+        {
+            String title = "Test Workflow";
+            String des = "Test";
+            HomeController controller = new HomeController();
+            Guid? result = controller.Create(title, des);
+            Assert.IsNull(result);
         }
     }
 }
