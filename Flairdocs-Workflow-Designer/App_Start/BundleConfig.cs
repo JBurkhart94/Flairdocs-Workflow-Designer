6b51e84c06edb46c1396a6cba3be1be45684279c
mkoch17 <koch.382@osu.edu>
2017-11-14 14:49:32 -0500

    Added Basic Drag and Drop Functionality

--- Flairdocs-Workflow-Designer/App_Start/BundleConfig.cs
+++ Flairdocs-Workflow-Designer/App_Start/BundleConfig.cs
@@ -14,6 +14,9 @@ namespace Flairdocs_Workflow_Designer
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
+            bundles.Add(new ScriptBundle("~/bundles/jqueryUI").Include(
+                        "~/Scripts/jquery-ui-{version}.js"));
+
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
